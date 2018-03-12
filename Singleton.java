ad026d2f7a1f7487253c29794d7ba845ad22b585
Yiyang Zhu <ererandkeke@gmail.com>
2018-03-12 15:00:13 -0400

    implementation of Singleton

---
+++ Singleton.java
@@ -0,0 +1,18 @@
+class Singleton{
+    static Singleton instance = null;
+    
+    Singleton getInstance(){
+        if(instance == null){
+            instance = new Singleton();
+        }
+        return instance;
+    }
+    
+    public static void main(String[] args){
+        Singleton s = new Singleton();
+        Singleton s2 = new Singleton();
+        System.out.println(s.getInstance());
+        System.out.println(s2.getInstance());
+    }
+}
+        
